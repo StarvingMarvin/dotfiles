72038f9a78c993df08dc49e75b0b0ab28fb580f5
Luka <slartibartfast@inbox.com>
2009-06-30 11:39:25 +0200

    nadrkotine sa workspace-ovima

--- .vimrc
+++ .vimrc
@@ -2,9 +2,12 @@ syntax on
 set nowrap
 set list listchars=tab:»·,trail:·,extends:»
 set autoindent
+set smartindent
+set pastetoggle=<F2>
 set number
-set cursorline
 set hlsearch
 set incsearch
 set tabstop=4
 set background=dark
+set expandtab
+set mouse=a
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,25 +4,81 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeysP)
+import XMonad.Layout.Tabbed
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.SimpleFloat
+import XMonad.Util.Themes
+import Data.Maybe
+--import XMonad.Prompt
+--import XMonad.Prompt.Shell
+--import XMonad.Prompt.Ssh
+--import XMonad.Prompt.Theme
+--import XMonad.Prompt.Window
+--import XMonad.Prompt.XMonad
+--import XMonad.Prompt.Layout
+--import XMonad.Prompt.Man
+--import XMonad.Prompt.AppendFile
+--import XMonad.Prompt.Input
 import System.IO
 
 shortcuts = 
-    [("M-x f", spawn "firefox")]
+    [ ("M-x f", spawn "firefox")
+--	, ("M-s", shellPrompt defaultXPConfig)
+	]
 
 positioning = composeAll
-    [className =? "Firefox"     --> doShift "2"]
+    [ className =? "Firefox"     --> (doShift $ getWorkspace "web")
+    , className =? "KCalc"       --> (doShift $ getWorkspace "float")
+    , className =? "KCalc"       --> doFloat
+   -- , className =? "CGoban"     --> doFloat
+    ]
+
+-- tabbed shrinkText (theme smallClean)
+myLayouts = onWorkspace (getWorkspace "term")  (avoidStruts $ simpleTabbed) 
+            $ onWorkspace (getWorkspace "float")  simpleFloat
+            $ (avoidStruts $ layoutHook defaultConfig )
+
+myWorkspaces = ["term", "web", "dev", "file", "doc", "float"]
+
+enumeratedWorkspaces = 
+    zip (myWorkspaces ++ empty) [1..9]
+    where
+        empty = "" : empty
+
+showWorkspace (name, id) = show id ++ ":" ++ name
+
+showWorkspaces = 
+    map showWorkspace enumeratedWorkspaces
+
+getWorkspace name = showWorkspace ws
+    where ws = (name, fromMaybe 0 $ lookup name enumeratedWorkspaces)
+
+--promptConfig = defaultXPConfig
+--    { font = "-*-terminus-normal-*-*-*-12-*-*-*-*-*-*-*"
+--	, bgColor = "black"
+--	, fgColor = "#FFFFFF"
+--	, fgHLight = "#3377AA"
+--	, bgHLight = "#000000"
+--	, borderColor = "#444444"
+--	, promptBorderWidth = 1
+--	, position = Bottom
+--	, height = 16
+--	, historySize = 50
+--	}
 
 main = do
     xmproc <- spawnPipe "xmobar /home/luka/.xmonad/xmobarrc"
     
     xmonad $ defaultConfig
-        { terminal           = "urxvt"
-        , manageHook         = positioning <+> manageDocks <+> manageHook defaultConfig
-        , layoutHook         = avoidStruts  $  layoutHook defaultConfig
+        { manageHook         = positioning <+> manageDocks <+> manageHook defaultConfig
+-- avoidStruts  $  layoutHook defaultConfig
+        , layoutHook         = avoidStruts $ layoutHook defaultConfig
         , logHook            = dynamicLogWithPP $ xmobarPP
             { ppOutput       = hPutStrLn xmproc
-            , ppTitle        = xmobarColor "green" "" . shorten 50
+            , ppTitle        = xmobarColor "green" "" . shorten 60
             }
+        , workspaces = showWorkspaces
+        , terminal           = "urxvt"
         , normalBorderColor  = "#cccccc"
         , focusedBorderColor = "#778800" 
         } `additionalKeysP` shortcuts
